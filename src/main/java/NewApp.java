917abde629db894f6ca580234bdaf346bb729814
Alexandr Sashchenko <santys87@gmail.com>
2019-12-02 01:23:28 +0200

    added "ask for greeting" class

---
+++ src/main/java/NewApp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class NewApp {
+    public static void main (String[] args ) {
+        System.out.println("Please enter the greeting");
+        System.out.println("You said" + Worker.setGreeting() + "so have a nice day!");
+    }
+}
+
+class Worker {
+    static String setGreeting() {
+        Scanner track = new Scanner(System.in);
+        return track.nextLine();
+    }
+}
